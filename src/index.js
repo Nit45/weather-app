b971974ba1be531e7b13415899b5a9ccd0573ba1
niteshprojects <agarwalnitesh0408@gmail.com>
2020-06-07 12:26:48 +0530

    setup app for heroku

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ app.get('*',(req,res)=>{
 
 })
 
-
-app.listen(3000,()=>{
-    console.log("server is running on 3000")
+const port= process.env.PORT || 3000 
+app.listen(port,()=>{
+    console.log("server is running on:"+port)
 })
